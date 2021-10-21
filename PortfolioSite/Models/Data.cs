d68b31b7cc8bb6ddd318c780958085bd138ebec0
Henrik Levin Bochesa <henr15r4@edu.ucl.dk>
2021-10-21 07:58:51 +0200

    removed deadlinks

--- PortfolioSite/Models/Data.cs
+++ PortfolioSite/Models/Data.cs
@@ -28,8 +28,8 @@ namespace PortfolioSite.Models
         {
             return new List<Portfolio>
             {
-                new Portfolio{ Id=1, Link="https://github.com/bochesa/ScoreBoard", Name="ScoreBoard", Description="beskrivelse", Picture="/images/pic01.jpg"},
-                new Portfolio{ Id=2, Link="https://github.com/bochesa/PumpDashboard", Name="Pump Dashboard", Description="beskrivelse2", Picture="/images/Work_01.jpg"},
+               // new Portfolio{ Id=1, Link="https://github.com/bochesa/ScoreBoard", Name="ScoreBoard", Description="beskrivelse", Picture="/images/pic01.jpg"},
+                // new Portfolio{ Id=2, Link="https://github.com/bochesa/PumpDashboard", Name="Pump Dashboard", Description="beskrivelse2", Picture="/images/Work_01.jpg"},
                 new Portfolio{ Id=3, Link="https://github.com/bochesa/CykelKlubben", Name="Cykelklubben", Description="beskrivelse3", Picture="/images/Work_02.png"},
                 //new Portfolio{ Id=4, Link="", Name="Test4", Description="beskrivelse4", Picture="/images/Work_04.jpg"},
                 //new Portfolio{ Id=5, Link="", Name="Test5", Description="beskrivelse5", Picture="/images/Work_05.jpg"},
